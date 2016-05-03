736851a042c94c4876dc6132640e71811519b389
Patrick Smith <pgwsmith@gmail.com>
2016-05-03 22:47:12 +1000

    Can publish files

---
+++ publishFile.js
@@ -0,0 +1,33 @@
+const R = require('ramda')
+const Url = require('url')
+const Fs = require('fs')
+const Path = require('path')
+const fetch = require('node-fetch')
+const hashFile = require('./hashFile')
+
+function publishFile({ host, account, filePath, hash }) { 
+    return (
+        R.isNil(hash) ? hashFile(filePath) : Promise.resolve(hash)
+    ).then((hash) => {
+            console.log('hash', hash, 'filePath', filePath)
+            const url = Url.format({
+                protocol: R.ifElse(
+                    R.pipe(
+                        R.split(':'),
+                        R.head,
+                        R.equals('localhost')
+                    ),
+                    R.always('http'),
+                    R.always('https')
+                )(host),
+                host,
+                pathname: `@${account}/${hash}`
+            })
+            console.log('url', url)
+            const stream = Fs.createReadStream(filePath)
+            return fetch(url, { method: 'POST', body: stream })
+        })
+        .then((response) => response.json())
+}
+
+module.exports = publishFile
