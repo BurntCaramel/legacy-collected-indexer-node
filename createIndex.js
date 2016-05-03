b5d017879baba1343e2f645d8fb099d8b8a68811
Patrick Smith <pgwsmith@gmail.com>
2016-05-03 17:19:19 +1000

    Merge existing index when rehashing

--- createIndex.js
+++ createIndex.js
@@ -1,11 +1,27 @@
 const R = require('ramda')
+const readIndex = require('./readIndex')
 const hashDirectory = require('./hashDirectory')
 
 const groupByHash = R.reduceBy((acc, item) => acc.concat(item.name), [], R.prop('hash'))
 
-const createIndex = R.pipeP(
-    hashDirectory,
-    groupByHash
+const createIndex = R.converge(
+    (...promises) => (
+        Promise.all(promises)
+            .then(R.mergeAll)
+    )
+    /*R.pipeP(
+        R.unapply(Promise.all),
+        R.mergeAll
+    )*/, [
+        R.pipeP(
+            readIndex
+        ),
+        R.pipeP(
+            hashDirectory,
+            groupByHash,
+            R.objOf('hashes')
+        )
+    ]
 )
 
 module.exports = createIndex
