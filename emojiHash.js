a003b451abb2fe60c297741c9d6df1812b00c3dc
Patrick Smith <pgwsmith@gmail.com>
2016-05-05 18:20:34 +1000

    Display emoji when hashing directory

---
+++ emojiHash.js
@@ -0,0 +1,12 @@
+const R = require('ramda')
+const base16ToEmoji = require('./base16ToEmoji')
+
+const emojiHash = R.pipe(
+    R.take(7),
+    R.map(R.pipe(
+        R.toLower,
+        R.prop(R.__, base16ToEmoji)
+    ))
+)
+
+module.exports = emojiHash
